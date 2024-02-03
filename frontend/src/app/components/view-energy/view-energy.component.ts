bc15676ebd7134f396907aaf64c1559886311db2
AhmedBeja <medouzzo13@gmail.com>
2024-02-03 17:09:12 +0100

    commit chargement deuxième chart ok

--- frontend/src/app/components/view-energy/view-energy.component.ts
+++ frontend/src/app/components/view-energy/view-energy.component.ts
@@ -35,11 +35,10 @@ export class ViewEnergyComponent implements OnInit {
   chartDataLabels: ApexDataLabels = {
     enabled: true
   };
-
+  initialChartTitle: string = 'Energies';
   ngOnInit(): void {
     this.Energies_typeService.getEnergies_type().subscribe(data => {
       this.energies_type = data;
-
       // Map the data for the chart series
       this.chartSeries = this.energies_type.map((energy: any) => energy.Energie_totale);
       
@@ -48,7 +47,25 @@ export class ViewEnergyComponent implements OnInit {
   });
 }
 onChartLoad(chart: any): void {
-  console.log('Chart loaded:', chart.target.parentElement.getAttribute("data:realIndex"));
+  const clickedPart = chart.target.parentElement.getAttribute("data:realIndex");
+  const clickedEnergyDetails = this.energies_type[clickedPart];
+  this.Energies_meanService.getEnergies_meanByType(clickedEnergyDetails.type).subscribe(filteredData => {
+    for (let i = 0; i < filteredData.length; i++) {
+      const element = filteredData[i];
+      this.loadEnergiesMean(element.family);
+    }
+  });
+}
+
+loadEnergiesMean(clickedEnergyType: any): void {
+  this.initialChartTitle = 'Moyen de production';
+  this.Energies_meanService.getEnergies_meanByType(clickedEnergyType).subscribe(newChartData => {
+
+    // Met à jour les propriétés du composant avec les nouvelles données
+    this.chartSeries = newChartData.map((energy: any) => energy.pourcentage);
+    this.chartLabels = newChartData.map((energy: any) => energy.type);
+    this.chartTitle.text = this.initialChartTitle;
+  });
 }
 
 }
