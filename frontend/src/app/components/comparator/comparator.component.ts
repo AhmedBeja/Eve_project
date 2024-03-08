b67632ad595ed706fd918d83219487680ecbbcad
Apolline <apolline.kozlovsky@free.fr>
2024-03-08 11:19:50 +0100

    tableau comparateur

--- frontend/src/app/components/comparator/comparator.component.ts
+++ frontend/src/app/components/comparator/comparator.component.ts
@@ -11,12 +11,13 @@ import { EnergiesService } from 'src/app/energies.service';
     @ViewChild('energy1Select') energy1Select!: ElementRef;
     @ViewChild('energy2Select') energy2Select!: ElementRef;
     result: string = '';
+    energies_list: any | undefined;
+    energy1Details: any | undefined;
+    energy2Details: any | undefined;
 
     constructor(
       private EnergiesService: EnergiesService
     ) {}
-
-    energies_list: any | undefined;
 
     ngOnInit(): void {
       this.EnergiesService.getEnergies().subscribe((data: any[]) => {
@@ -30,24 +31,21 @@ import { EnergiesService } from 'src/app/energies.service';
       this.result = `Vous avez sélectionné : ${energy1Value} et ${energy2Value}`;
       
       // Récupérer les détails de l'énergie 
-  const energy1Details = this.energies_list.find((energy: any) => energy.name === energy1Value);
-  const energy2Details = this.energies_list.find((energy: any) => energy.name === energy2Value);
+  this.energy1Details = this.energies_list.find((energy: any) => energy.name === energy1Value);
+  this.energy2Details = this.energies_list.find((energy: any) => energy.name === energy2Value);
 
-  if (energy1Details && energy2Details) {
-    const energy1Cost = energy1Details.lcoe;
-    const energy2Cost = energy2Details.lcoe;
+  if (this.energy1Details && this.energy2Details) {
 
-    if (energy1Cost < energy2Cost) {
-      this.result = `${energy1Details.name} a un LCOE inférieur à ${energy2Details.name} (${energy1Details.lcoe}<${energy2Details.lcoe})`;
-    } else if (energy1Cost > energy2Cost) {
-      this.result = `${energy1Details.name} a un LCOE supérieur à ${energy2Details.name} (${energy1Details.lcoe}>${energy2Details.lcoe})`;
+    if (this.energy1Details.lcoe < this.energy2Details.lcoe) {
+      this.result = `${this.energy1Details.name} a un LCOE inférieur à ${this.energy2Details.name} (${this.energy1Details.lcoe}<${this.energy2Details.lcoe})`;
+    } else if (this.energy1Details.lcoet > this.energy2Details.lcoe) {
+      this.result = `${this.energy1Details.name} a un LCOE supérieur à ${this.energy2Details.name} (${this.energy1Details.lcoe}>${this.energy2Details.lcoe})`;
     } else {
-      this.result = `${energy1Details.name} et ${energy2Details.name} ont le même LCOE (${energy1Details.lcoe})`;
+      this.result = `${this.energy1Details.name} et ${this.energy2Details.name} ont le même LCOE (${this.energy1Details.lcoe})`;
     }
   } else {
     this.result = 'Impossible de comparer les énergies sélectionnées.';
   }
-  console.log(energy1Value)
-}
-
+  console.log(this.energy1Details)
+  }
   }
